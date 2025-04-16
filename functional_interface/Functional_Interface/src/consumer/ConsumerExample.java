279154b486bb7f61c217393b6109f3203ebbbb77
isaque <isaquenobrec@gmail.com>
2025-04-16 12:22:57 -0300

    primeira stream: Consumer - alteração

--- functional_interface/Functional_Interface/src/consumer/ConsumerExample.java
+++ functional_interface/Functional_Interface/src/consumer/ConsumerExample.java
@@ -15,13 +15,35 @@ import java.util.function.Consumer;
 public class ConsumerExample {
     
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+       /* List<Integer> numeros = Arrays.asList(1,2,3,4,5);
         Consumer<Integer> imprimirNumeroPar = numero -> {
             if (numero %2 ==0){
                 System.out.println(numero);
             }
         };
-      numeros.stream().forEach(imprimirNumeroPar);
+      numeros.stream().forEach(imprimirNumeroPar);*/
+       
+      /*List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+       
+        
+      numeros.stream().forEach(new Consumer<Integer>() {
+          @Override
+          public void accept(Integer t) {
+              if(t%2==0){
+                  System.out.println(t);
+              }
+          }
+      });*/
+      
+     
+     List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+     
+       numeros.stream().forEach((Integer t) -> {
+           if(t%2==0){
+               System.out.println(t);
+           }
+      });
+      
         
     }
     
