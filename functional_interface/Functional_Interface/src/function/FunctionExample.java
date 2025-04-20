da25696b0e7cf41718d1df19663226e08475b6ef
isaque <isaquenobrec@gmail.com>
2025-04-19 22:37:05 -0300

    stream_api - interface Function - com lambda

--- functional_interface/Functional_Interface/src/function/FunctionExample.java
+++ functional_interface/Functional_Interface/src/function/FunctionExample.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 //terceira aula sobre function interface - aula 103 das anotações
 public class FunctionExample {
     public static void main(String[] args) {
-        List <Integer> numeros = Arrays.asList(1,2,3,4,5);
+        /*List <Integer> numeros = Arrays.asList(1,2,3,4,5);
         
         Function<Integer, Integer> dobrar = numero -> numero * 2;
         
@@ -26,7 +26,51 @@ public class FunctionExample {
          .map(dobrar)
          .collect(Collectors.toList());
 
-numerosDobrados.forEach(n -> System.out.println(n));        
+//numerosDobrados.forEach(n -> System.out.println(n)); tanto essa forma quanto a debaixo funcionam do mesmo jeito
+numerosDobrados.forEach(System.out::println); //method reference*/
+
+
+
+// Entendendo a function
+
+/*List <Integer> numeros = Arrays.asList(1,2,3,4,5);
+        
+        Function<Integer, Integer> dobrar = numero -> numero * 2;
+        
+        List<Integer> numerosDobrados = numeros.stream()
+         
+                .map(
+                        
+                        new Function<Integer, Integer>() {
+    @Override
+    public Integer apply(Integer t) {
+        return t * 2;  
+    }
+}
+                )
+                .collect(Collectors.toList());
+        
+        
+        
+        numerosDobrados.forEach(System.out::println);*/
+
+
+
+
+
+// usando a função lambda
+List <Integer> numeros = Arrays.asList(1,2,3,4,5);
+        
+        Function<Integer, Integer> dobrar = numero -> numero * 2;
+        
+        List<Integer> numerosDobrados = numeros.stream()
+         
+                .map(n->n*2)
+                .collect(Collectors.toList());
+        
+        numerosDobrados.forEach(System.out::println);
+
+
     }
 
    
