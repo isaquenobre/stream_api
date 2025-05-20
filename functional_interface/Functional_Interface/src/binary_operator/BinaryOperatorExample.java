9ab8a37abf8eb54ab4c998edf8cd7d35c9fba275
isaque <isaquenobrec@gmail.com>
2025-05-20 16:46:27 -0300

    interface Binary Operation

---
+++ functional_interface/Functional_Interface/src/binary_operator/BinaryOperatorExample.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package binary_operator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+/**
+ *
+ * @author Windows 10 PRO
+ */
+public class BinaryOperatorExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        
+        BinaryOperator<Integer> somar = (num1,num2) -> num1 + num2;
+        
+        int resultado = numeros.stream()
+                .reduce(0, somar);
+        
+        System.out.println("A soma dos numeros é: " + resultado);
+    }
+    
+}
