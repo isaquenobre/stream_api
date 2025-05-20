c9c4acf67222ade094c85f011b0bb61648c7ae8a
isaque <isaquenobrec@gmail.com>
2025-05-20 19:48:50 -0300

    interface Binary Operation-final

--- functional_interface/Functional_Interface/src/binary_operator/BinaryOperatorExample.java
+++ functional_interface/Functional_Interface/src/binary_operator/BinaryOperatorExample.java
@@ -14,12 +14,36 @@ import java.util.function.BinaryOperator;
  */
 public class BinaryOperatorExample {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        /*List<Integer> numeros = Arrays.asList(1,2,3,4,5);
         
         BinaryOperator<Integer> somar = (num1,num2) -> num1 + num2;
         
         int resultado = numeros.stream()
                 .reduce(0, somar);
+        
+        System.out.println("A soma dos numeros é: " + resultado);*/
+        
+         /*List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        
+        BinaryOperator<Integer> somar = (num1,num2) -> num1 + num2;
+        
+        int resultado = numeros.stream()
+                .reduce(0, new BinaryOperator<Integer>() {
+             @Override
+             public Integer apply(Integer n1, Integer n2) {
+                 return n1+n2;
+             }
+                });
+        
+        System.out.println("A soma dos numeros é: " + resultado);*/
+        
+        
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        
+        BinaryOperator<Integer> somar = (num1,num2) -> num1 + num2;
+        
+        int resultado = numeros.stream()
+                .reduce(0,(n1,n2)-> n1+n2);
         
         System.out.println("A soma dos numeros é: " + resultado);
     }
