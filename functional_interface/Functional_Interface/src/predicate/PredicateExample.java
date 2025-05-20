1ac8655d1ea6b179551c28fcc8e81a8d66ce4047
isaque <isaquenobrec@gmail.com>
2025-05-20 04:13:56 -0300

    functional_Predacte

---
+++ functional_interface/Functional_Interface/src/predicate/PredicateExample.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package predicate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ *
+ * @author Windows 10 PRO
+ */
+public class PredicateExample {
+    public static void main(String[] args) {
+        /*List<String> palavras = Arrays.asList("java","kotlin", "python", "javascript","c","go","ruby");
+        
+        Predicate <String> maisDeCincoCaracteres = palavra->palavra.length()>5;
+        
+        palavras.stream()
+                .filter(maisDeCincoCaracteres)
+                .forEach(System.out::println);*/
+        
+        //entendendo a logica do codigo
+        
+        List<String> palavras = Arrays.asList("java","kotlin", "python", "javascript","c","go","ruby");
+        
+        Predicate <String> maisDeCincoCaracteres = palavra->palavra.length()>5;
+        
+        palavras.stream()
+                .filter(
+                         new Predicate<String>(){
+            @Override
+            public boolean test(String p) {
+                return p.length()>5;
+                
+            }
+                         }                 
+                )
+              .forEach(System.out::println);
+                
+                
+    }
+    
+}
